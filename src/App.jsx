4dcaf9a248d772c9d69bfc395dcce7316bd7707f
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-15 16:56:39 +0530

    firsts commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>React Testing Project for Live like process</h1>
+      <h1>Deployment Process of react Js in Git Hub</h1>
+    </>
+  );
+};
+
+export default App;
